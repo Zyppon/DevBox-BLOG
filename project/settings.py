b354647d2326d43949efbea7afe9a9b99498dda5
Zyppon <mcpezyppon@gmail.com>
2024-04-21 00:12:03 +0200

    commit

--- project/settings.py
+++ project/settings.py
@@ -92,12 +92,26 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+   # 'default': {
+       # 'ENGINE': 'django.db.backends.sqlite3',
+       # 'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'GnWmxzmVNEESYsvuYSJkSyiyeLZJJPtT',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '25996',
     }
 }
+
 
 
 # Password validation
